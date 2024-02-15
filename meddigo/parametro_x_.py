e6fd590b8d76fb1fcd5884b2c849aa7c3f39eae0
Megiddo <juancholo182005@gmail.com>
2024-02-15 16:11:02 -0600

    Terminando mi tarea

--- meddigo/parametro_x_.py
+++ meddigo/parametro_x_.py
@@ -1,1 +1,12 @@
-print("Hola mundo!")
+def tabulacion():
+    tabla=[]
+    x=-4
+    while(x<=2):
+        tabla.append((248*(x**2)+28*(x)-89))
+        x+=0.25
+    print(tabla)
+
+tabulacion()
+
+
+
--- meddigo/tabulacion.py
+++ meddigo/tabulacion.py
@@ -1,1 +1,13 @@
-print("Hola mundo!")
+import base64
+print("Ingresa tu texto:")
+texto_ingresado=input("> ")
+codificado=texto_ingresado.encode()
+print("Que deseas hacer:")
+print("1-pasar texto a base 64")
+print("2-pasar a 16 a base 16")
+opcion=int(input("> "))
+if opcion == 1: 
+    codificado=base64.standard_b64encode(codificado)
+elif opcion == 2:
+    codificado=base64.b16encode(codificado)
+print(codificado)
