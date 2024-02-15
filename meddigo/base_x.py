0214f8e7722fd1f01ddca197911a4cbe7453d0f7
Megiddo <juancholo182005@gmail.com>
2024-02-15 15:25:32 -0600

    haciendo tarea en mi rama

--- meddigo/base_x.py
+++ meddigo/base_x.py
@@ -1,1 +1,5 @@
-print("Hola mundo!")
+import math
+print("Ingresa el valor de x:")
+base_x=int(input("> "))
+base_x=(3+(base_x*math.pi)+math.sin((2*(base_x**2))))/(2*(math.fabs(base_x)))
+print("El valor de su funcion de acuerdo a el dato ingresado es:",base_x)
