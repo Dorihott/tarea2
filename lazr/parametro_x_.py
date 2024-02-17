7cbc31b0c575a13fc694a6f0ea5e63c87ba4072b
LAZR <zluisangel03@gmail.com>
2024-02-16 20:52:27 -0600

    base

--- lazr/parametro_x_.py
+++ lazr/parametro_x_.py
@@ -1,1 +1,11 @@
-print("Hola mundo!")
+def calcular_funcion(x):
+    return 248 * x**2 + 28 * x - 89
+
+print(" x   |   f(x)")
+print("--------------")
+
+x = -4.0
+while x <= 2.0:
+    resultado = calcular_funcion(x)
+    print("{:5.2f} | {:7.2f}".format(x, resultado))
+    x += 0.25
--- lazr/tabulacion.py
+++ lazr/tabulacion.py
@@ -1,1 +1,14 @@
-print("Hola mundo!")
+import math
+
+def calcular_funcion(x):
+    sin_2x_cuadrado = math.sin(2 * x**2)
+    valor_absoluto_x = abs(x)
+    resultado = 3 + x * math.pi + sin_2x_cuadrado / (2 * valor_absoluto_x)
+
+    return resultado
+
+
+print("Ingresa el valor de x estupido")
+x = int(input())
+resultado = calcular_funcion(x)
+print("El resultado de x es", x, "es:", resultado, " ponte a estuiar cabron")
